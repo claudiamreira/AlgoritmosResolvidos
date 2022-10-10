885d18871e4c4fb9ebe09d0c4c22531e09c0d38f
Claudia Moreira <107289244+claudiamreira@users.noreply.github.com>
2022-10-10 20:57:24 -0300

    Update Parcela.java

--- algoritmos/Parcela.java
+++ algoritmos/Parcela.java
@@ -1,5 +1,6 @@
 package algoritmos;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Parcela {
@@ -9,7 +10,10 @@ public class Parcela {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		float valor, juros, total;
+		DecimalFormat df = new DecimalFormat(".00");
+		
+		float valor;
+		double parc, juros, total;
 		int parcelas;
 		
 		
@@ -19,35 +23,68 @@ public class Parcela {
 		System.out.println("Digite a quantidade de parcelas: ");
 		parcelas = sc.nextInt();
 		
-		juros = valor * 0.10f;
-		total = valor + juros;
 		
 		switch(parcelas) {
 			case 1:
 				System.out.println("Você pagará R$" + valor + " á vista!");
 				break;
 			case 2:
-				System.out.println("Você pagará R$" + valor + " parcelado em " + parcelas 
+				parc = valor / 2;
+				System.out.println("Você pagará R$" + parc + " parcelado em " + parcelas 
 						+ "x sem juros");
 				break;
 			case 3:
-				System.out.println("Você pagará R$" + valor + " parcelado em " + parcelas 
+				parc = valor / 3;
+				System.out.println("Você pagará R$" + parc + " parcelado em " + parcelas 
 						+ "x sem juros");
 				break;
 			case 4:
-				System.out.println("Você pagará R$" + valor + " parcelado em " + parcelas 
+				parc = valor / 4;
+				System.out.println("Você pagará R$" + parc + " parcelado em " + parcelas 
 						+ "x sem juros");
 				break;
 			case 5:
-				System.out.println("Você pagará R$" + valor + " parcelado em " + parcelas 
+				parc = valor /5;
+				System.out.println("Você pagará R$" + parc + " parcelado em " + parcelas 
 						+ "x sem juros");
 				break;
 			case 6:
-				System.out.println("Você pagará R$" + total + " parcelado em " + parcelas
-						+ "x com 10% de juros!");
+				juros = (valor * 0.06);
+				total = valor + juros;
+				parc = total / 6;
+				System.out.println("Você pagará R$" + df.format(parc) + " parcelado em " + parcelas
+						+ "x com acréscimo de juros!");
+				break;
+			case 7:
+				juros = (valor * 0.07);
+				total = valor + juros;
+				parc = total / 7;
+				System.out.println("Você pagará R$" + df.format(parc) + " parcelado em " + parcelas
+						+ "x com acréscimo de juros!");
+				break;
+			case 8:
+				juros = (valor * 0.08);
+				total = valor + juros;
+				parc = total / 8;
+				System.out.println("Você pagará R$" + df.format(parc) + " parcelado em " + parcelas
+						+ "x com acréscimo de juros!");
+				break;
+			case 9:
+				juros = (valor * 0.09);
+				total = valor + juros;
+				parc = total / 9;
+				System.out.println("Você pagará R$" + df.format(parc) + " parcelado em " + parcelas
+						+ "x com acréscimo de juros!");
+				break;
+			case 10:
+				juros = (valor * 0.10);
+				total = valor + juros;
+				parc = total / 10;
+				System.out.println("Você pagará R$" + df.format(parc) + " parcelado em " + parcelas
+						+ "x com acréscimo de juros!");
 				break;
 			default:
-				System.out.println("Quantidade de parcelas não permitido!");
+				System.out.println("Quantidade de parcelas não permitida!");
 				
 		}
 	}
