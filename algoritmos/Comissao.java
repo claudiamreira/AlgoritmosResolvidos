a706e98937e1501649763b7fa388dc510193df8c
Claudia Moreira <107289244+claudiamreira@users.noreply.github.com>
2022-10-10 22:48:00 -0300

    Update Comissao.java

--- algoritmos/Comissao.java
+++ algoritmos/Comissao.java
@@ -14,7 +14,7 @@ public class Comissao {
 		double salarioComis, comissao;
 		int qtdVendas;
 		
-		System.out.println("Informe o nome do vendedor: ");
+		System.out.println("Informe o nome do(a) vendedor(a): ");
 		nome = scan.next();
 		
 		System.out.println("Informe o seu salário fixo mensal: ");
@@ -23,14 +23,14 @@ public class Comissao {
 		System.out.println("Informe a quantidade de vendas realizada no mês: ");
 		qtdVendas = scan.nextInt();
 		
-		comissao = (qtdVendas * 0.15);
-		salarioComis = salarioFixo + comissao;
+		comissao = qtdVendas * 1.15;
+		salarioComis = comissao + salarioFixo;
 		
-		System.out.println("O nome do vendedor é: " + nome +
-				" seu salário mensal é de: " + salarioFixo +
-				" a quantidade de vendas realizada no mês é de: " + qtdVendas +
-				" sua comissão é de: " + comissao +
-				" o valor total a ser recebido é de: " + salarioComis);
+		System.out.println("O nome do(a) vendedor(a) é: " + nome);
+		System.out.println("Seu salário é de R$" + salarioFixo);
+		System.out.println("A quantidade de vendas realizada no mês é de: " + qtdVendas);
+		System.out.println("Sua comissão é de R$" + comissao);
+		System.out.println("O valor total a ser recebido é de R$" + salarioComis);
 	}
 
 }
